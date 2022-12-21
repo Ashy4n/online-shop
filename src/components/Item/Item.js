d93064768a0acac7e30b68a845bc31eaea2008fa
Mateusz Dworowy <mateuszdworowy@gmail.com>
2022-12-21 18:11:54 +0100

    few minor item changes

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,17 +5,19 @@ const Item = (props) => {
     return (
         <div className={styles.itemContainer}>
             <img src={props.imgUrl}></img>
-            <div>
+            <div className={styles.itemInfo}>
                 <h1>
                     {props.name}
                 </h1>
                 <p>
                     {props.description}
                 </p>
-                <p>
-                    {props.price}
-                </p>
-                <button><i class="fa-solid fa-cart-shopping"></i></button>
+                <div>
+                    <p>
+                        ${props.price}
+                    </p>
+                    <button><i class="fa-solid fa-cart-shopping"></i></button>
+                </div>
             </div>
         </div>
     )
